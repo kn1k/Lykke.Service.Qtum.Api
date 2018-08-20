c43e6b4616e0e6bef21a3fb69c3ec79b9987d976
bkarabanov <bkarabanov@ics.perm.ru>
2018-08-20 18:51:06 +0500

    #LWDEV-8983 regenerate with correct name

---
+++ client/Lykke.Service.Qtum.Api.Client/Qtum.ApiServiceClientSettings.cs
@@ -0,0 +1,14 @@
+﻿using Lykke.SettingsReader.Attributes;
+
+namespace Lykke.Service.Qtum.Api.Client 
+{
+    /// <summary>
+    /// Qtum.Api client settings.
+    /// </summary>
+    public class QtumApiServiceClientSettings 
+    {
+        /// <summary>Service url.</summary>
+        [HttpCheck("api/isalive")]
+        public string ServiceUrl {get; set;}
+    }
+}
---
+++ src/Lykke.Service.Qtum.Api/Settings/AppSettings.cs
@@ -0,0 +1,11 @@
+﻿using JetBrains.Annotations;
+using Lykke.Sdk.Settings;
+
+namespace Lykke.Service.Qtum.Api.Settings
+{
+    [UsedImplicitly(ImplicitUseTargetFlags.WithMembers)]
+    public class AppSettings : BaseAppSettings
+    {
+        public QtumApiSettings QtumApiService { get; set; }
+    }
+}
